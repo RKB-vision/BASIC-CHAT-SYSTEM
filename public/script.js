86542f3aef3677fb59873e6fea32ac683a0610ed
RKB-vision <rkbsapkotavision@gmail.com>
2026-02-13 12:38:55 +0530

    ->UPDATED UI INTERFACE FOR SENDER(msg on right)  and  EVERYONE IN THE SERVER SO THAT THEY SEE WHO IS TYPING

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
 const socket = io();//is this prebuilt?
 
 // Logic: We emit an event called 'message' with a string
-
-socket.emit('message', 'Hello Server! Can you hear me?');
+let username=""
+while (!username||username.trim().length<3){
+ username=prompt("Enter your name")}
+socket.emit('username',username);
 
 const chatForm = document.getElementById('chat-form');
 const messageInput = document.getElementById('message-input');
@@ -11,7 +13,8 @@ const messageArea = document.getElementById('message-area');
 // 1. Sending a message (The "Mouth")
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault(); // Stop page refresh
-    const msg = messageInput.value;
+    const msg = messageInput.value.trim();
+    if (!msg) return; // Ignore empty messages
     
     socket.emit('chat-message', msg); // Send to server
     messageInput.value = ''; // Clear input
@@ -19,12 +22,17 @@ chatForm.addEventListener('submit', (e) => {
 
 // 2. Receiving a message (The "Ears")
 socket.on('message-to-all', (data) => {
-    console.log('The server says:', data);
+    console.log('The server says:', data.msg);
 
     // Create a new div for the message
     const messageDiv = document.createElement('div');
-    messageDiv.classList.add('msg');
-    messageDiv.innerText = data;
+    if(data.id===socket.id){
+    messageDiv.classList.add('mymsg');
+    }
+    else{
+        messageDiv.classList.add('othermsg')
+    }
+    messageDiv.innerText = `${data.user} : `+data.msg;
     
     // Put it in the message area
     messageArea.appendChild(messageDiv);
--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -2,12 +2,23 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log("New User joined just now")
         // LISTEN: When this specific user sends a 'message'
+        socket.on("username",(data)=>{
+            socket.user=data
+        })
         socket.on('chat-message', (data) => {
             console.log('New message received:', data);
+            data_to_send={
+                user:socket.user,
+                msg:data,
+                id:socket.id
+            }
+            
+            //optimize user so that null  doesnot come.
+
 
             // BROADCAST: Send this message to EVERYONE connected
             // 'io.emit' talks to every single person in the chat room
-            io.emit('message-to-all', data);
+            io.emit('message-to-all',data_to_send);
         });
 
     });
